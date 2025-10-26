ede5caaf9869e153634f9d332efd3268fd1fe0fa
iestyn129 <iestyn129msm@gmail.com>
2025-10-26 02:29:56 +0000

    use IDEAs build system because Gradle doesn't let me interrupt the java program nicely

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,20 @@ plugins {
 group = "dev.iestyn129.femtoapi"
 version = "0.1.2"
 
+layout.buildDirectory = file("out")
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation(kotlin("reflect:2.2.20"))
+	implementation(kotlin("reflect:${kotlin.coreLibrariesVersion}"))
 	implementation(files("lib/TynLog.jar"))
+}
+
+tasks.jar {
+	archiveBaseName = project.name
+	archiveVersion = "v${project.version}"
 }
 
 kotlin {
